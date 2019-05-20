fe124a2332bc108cdc538a60081cd08026720838
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-20 11:08:19 +0900

    Implement: gkb

---
+++ gkb.py
@@ -0,0 +1,31 @@
+import os
+import redis
+from functools import reduce
+from operator import and_
+
+pool = redis.ConnectionPool.from_url(os.environ['REDIS_URL'], db=0, decode_responses=True)
+rc = redis.StrictRedis(connection_pool=pool)
+
+
+def sadd_pair(a, b):
+    rc.sadd(a, b)
+    rc.sadd(b, a)
+
+
+def set_values(key, values):
+    for value in values:
+        sadd_pair(key, value)
+
+
+def get_values(key):
+    if rc.exists(key):
+        return sorted(rc.smembers(key))
+
+
+def get_intersection(args):
+    if all(rc.exists(arg) for arg in args):
+        return reduce(and_, (rc.smembers(key) for arg in args for key in rc.keys(arg)))
+
+
+def get_keys():
+    return sorted(rc.keys())
--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
+from gkb import get_keys
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route('/keys')
+def keys():
+    results = '\n'.join(get_keys())
+    return results
 
 if __name__ == '__main__':
     app.run()
