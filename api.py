06024c2fea20476d58b27980f75f700069c7dd33
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-05-28 11:10:47 +0900

    Lint: flake8

--- api.py
+++ api.py
@@ -1,25 +1,29 @@
-import os
 from flask import Flask
 from flask import request
-from flask import json
 from flask import jsonify
 from gkb import get_keys
+from gkb import get_values
 from gkb import set_values
+
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
 
 @app.route('/keys')
 def keys():
     results = '\n'.join(get_keys())
     return results
 
+
 @app.route('/api/set_values', methods=['POST'])
 def api_set_values():
     posted = request.get_json()
-    keys = poested.keys()
+    keys = posted.keys()
     if len(keys) != 1:
         return jsonify({'Error': 'Specify only one key'})
     key = keys[0]
@@ -29,8 +33,9 @@ def api_set_values():
     if set_values(key, values):
         values = get_values(key)
         return jsonify({key: values})
-    else
+    else:
         return jsonify({'Error': 'Failed to connect to Database'})
+
 
 if __name__ == '__main__':
     app.run()
