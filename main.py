fcb2fb3a5e4967659854ebd30d62c8de3d4d7b52
1ntegrale9 <1ntegrale9uation@gmail.com>
2019-12-06 14:27:36 +0900

    Update

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import uvicorn
 from fastapi import FastAPI
 from gkb import get_keys
 
 app = FastAPI()
 
 
-@app.get('/tags')
+@app.get('/')
 def read_tags():
     return {'tags': get_keys()}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app)
