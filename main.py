5fd741269167552d63d91f7a40188a89cb15a0e6
1ntegrale9 <1ntegrale9uation@gmail.com>
2019-12-06 14:34:57 +0900

    Implement: 任意のタグに紐付くタグの取得

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from gkb import get_keys
+from gkb import get_keys, get_values
 
 app = FastAPI()
 
 
 @app.get('/')
-def read_tags():
+def read_tags_all():
     return {'tags': get_keys()}
+
+@app.get('/{tag}')
+def read_tags(tag: str):
+    return {'tags': get_values(tag)}
