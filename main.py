ac8bbd31ee9820db495f7d0514a09fcb5f43bf80
1ntegrale9 <1ntegrale9uation@gmail.com>
2019-12-06 14:18:54 +0900

    Fix: 起動方法を変更

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from gkb import get_keys
 
@@ -7,3 +8,7 @@ app = FastAPI()
 @app.get('/tags')
 def read_tags():
     return {'tags': get_keys()}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
